afbfbc8a9a76a3680b30c5000d947760c118cc62
sapa27 <natthapong.sripa@gmail.com>
2026-08-17 10:16:47 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+window.LIFE_APP_CONFIG = Object.freeze({
+  GAS_WEB_APP_URL: 'PASTE_YOUR_GAS_EXEC_URL_HERE',
+  BRIDGE_TIMEOUT_MS: 30000,
+  REQUEST_TIMEOUT_MS: 90000,
+  BRAND: Object.freeze({
+    COMPANY_NAME: 'บริษัท ชับบ์ ไลฟ์ แอสชัวรันซ์ จำกัด (มหาชน)',
+    DISPLAY_NAME: 'Chubb Life Thailand',
+    P0D_PROPOSAL_MODE: 'REFERENCE_SAFE',
+    BRAND_AUTHORIZED: false,
+    OFFICIAL_LOGO_URL: 'https://news.chubb.com/download/CHUBB_Logo_Black_RBG.png',
+    OFFICIAL_LOGO_ASSET_PAGE: 'https://news.chubb.com/logos',
+    LOGO_USAGE_GUIDE: 'https://news.chubb.com/usage-guide',
+    TRADEMARK_ATTRIBUTION: 'The Chubb logo is a registered trademark of Chubb Limited.'
+  })
+});
